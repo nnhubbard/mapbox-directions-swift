af5f7df6df910a03f9b3410cb05aeeb94178396e
Maarten Zonneveld <maartenzonneveld@live.nl>
2024-02-16 10:59:58 +0100

    iOS 12 SPM

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MapboxDirections",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "MapboxDirections",
